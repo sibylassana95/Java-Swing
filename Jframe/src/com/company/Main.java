af832dc4ae7be17ed38217c57b5ef1d68c27fdaf
Lassana siby <103085452+sibylassana95@users.noreply.github.com>
2022-05-25 00:49:15 +0000

    JFrame

---
+++ Jframe/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello World");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(250, 250);
+        frame.setVisible(true);
+    }
+}
